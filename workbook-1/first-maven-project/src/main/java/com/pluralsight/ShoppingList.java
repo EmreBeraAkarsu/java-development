ba2f286746ea3542d304fa8f6b1bad1eedf01d09
Emre Akarsu <akarsutech@gmail.com>
2024-09-25 13:25:55 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("apple");
+        System.out.println("pear");
+        System.out.println("pineapple");
+        System.out.println("strawberry");
+        System.out.println("rasberry");
+        System.out.println("blueberry");
+        System.out.println("melon");
+        System.out.println("cantelope");
+        System.out.println("dragon fruit");
+        System.out.println("onions");
+        System.out.println("parsley");
+
+    }
+}
