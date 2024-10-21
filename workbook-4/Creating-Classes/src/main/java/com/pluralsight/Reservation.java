98f178f1aba4400b53d208a868c6838005c6d96f
Emre Akarsu <akarsutech@gmail.com>
2024-10-21 14:00:31 -0400

    Created the HotelOperations exercise

---
+++ workbook-4/Creating-Classes/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+
+public class Reservation {
+
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+
+        double price;
+        if (roomType.equalsIgnoreCase("double")){
+            price = 124;
+        } else if (roomType.equalsIgnoreCase("king")) {
+            price = 139;
+
+        }else {
+            System.out.println("Invalid room type, cannot calculate the price");
+            return 0;
+        }
+
+        if (isWeekend){
+            price = price * 110/100;
+        }
+
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+
+}
