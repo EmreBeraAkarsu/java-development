02404dae7994beadc8c573b4a6f71c4f6493245f
Emre Akarsu <akarsutech@gmail.com>
2024-10-21 15:46:20 -0400

    created HotelOperations2 exercise

---
+++ workbook-4/HotelOperations2/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,75 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+    private double punchInTime;
+
+    public Employee(int employeeId, String name, String department, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = 0;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getTotalPay() {
+        if (hoursWorked <= 40) {
+            return hoursWorked * payRate;
+        } else {
+            return (40 * payRate) + ((hoursWorked - 40) * (payRate * 1.5));
+        }
+    }
+
+    public double getRegularHours() {
+        if (hoursWorked <= 40) {
+            return hoursWorked;
+        } else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeHours() {
+        if (hoursWorked > 40) {
+            return hoursWorked - 40;
+        } else {
+            return 0;
+        }
+    }
+
+  /*  public void punchIn(double time){
+        punchInTime = time;
+    }
+
+    public void punchOut(double time){
+        hoursWorked = time - punchInTime;
+    }*/
+
+    public void punchTimeCard(double punchIn, double punchOut){
+
+        hoursWorked = punchOut - punchIn;
+    }
+}
---
+++ workbook-4/HotelOperations2/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = false;
+        this.isDirty = false;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return !isOccupied && !isDirty;
+    }
+
+    public void checkIn(){
+        if (isDirty){
+            System.out.println("Cannot check into a dirty room!");
+            return;
+        }
+        isOccupied = true;
+        isDirty = true;
+    }
+
+    public void checkOut(){
+        isOccupied = false;
+    }
+
+    public void cleanRoom(){
+        isDirty = false;
+    }
+
+
+}
