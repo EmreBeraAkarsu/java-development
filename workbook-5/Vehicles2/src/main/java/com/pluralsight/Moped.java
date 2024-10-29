c7784c1311b3defb6862364f58a19ef5a56b7392
Emre Akarsu <akarsutech@gmail.com>
2024-10-29 09:36:03 -0400

    Created the Vehicles2 exercise

---
+++ workbook-5/Vehicles2/src/main/java/com/pluralsight/Moped.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+public class Moped extends Vehicle {
+    private int maxWeight;
+
+    public Moped(String model, String color, int topSpeed, int fuelCapacity, int numberOfPassengers, int cargoCapacity, int maxWeight) {
+        super(model, color, topSpeed, fuelCapacity, numberOfPassengers, cargoCapacity);
+        this.maxWeight = maxWeight;
+    }
+
+    public Moped() {
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void ride() {
+        System.out.println("Riding the moped.");
+    }
+}
